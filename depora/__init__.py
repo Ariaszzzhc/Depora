307aca854dd7bd2c7dcce7c27047fe34da23c461
Arias <489291461@qq.com>
2017-12-05 18:16:10 +0800

    feat: 支持MarkDown

--- depora/__init__.py
+++ depora/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flaskext.markdown import Markdown
 
 from depora import configs
 
@@ -8,5 +9,6 @@ app = Flask(__name__)
 app.config.from_object(configs.DevConfig)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+Markdown(app)
 
 __import__('depora.views')
